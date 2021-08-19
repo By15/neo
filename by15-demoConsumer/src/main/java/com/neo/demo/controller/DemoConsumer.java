546058ae9093b73965a075a941c67f66f7c96764
By15 <liuchong@ysstech.com>
2021-08-19 19:05:52 +0800

    调试

--- by15-demoConsumer/src/main/java/com/neo/demo/controller/DemoConsumer.java
+++ by15-demoConsumer/src/main/java/com/neo/demo/controller/DemoConsumer.java
@@ -1,11 +1,9 @@
 package com.neo.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import com.alibaba.fastjson.JSONObject;
 
 @RestController
 @RequestMapping("/demo")
---
+++ by15-gateway/src/main/java/com/neo/gateway/GatewayMain.java
@@ -0,0 +1,14 @@
+package com.neo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
+public class GatewayMain {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain.class,args);
+    }
+}
