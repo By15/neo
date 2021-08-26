3806f51ff1364bad366cfca3d1d238118c418de3
刘冲 <liuchong@ysstech.com>
2021-08-26 23:43:39 +0800

    update

--- by15-springeureka/src/main/java/com/neo/springeureka/EurekaApplication.java
+++ by15-springeureka/src/main/java/com/neo/springeureka/EurekaApplication.java
@@ -1,6 +1,5 @@
 package com.neo.springeureka;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
