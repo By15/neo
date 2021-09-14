4ba58ce080a6267ef8c5f868689fbcb10f79b08b
By15 <liuchong@ysstech.com>
2021-09-14 14:13:19 +0800

    调试

--- by15-User/src/main/java/com/neo/user/controller/UserBaseInfoController.java
+++ by15-User/src/main/java/com/neo/user/controller/UserBaseInfoController.java
@@ -1,9 +1,14 @@
 package com.neo.user.controller;
 
-import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+
+/**
+ * @author By15
+ * @since 2021/9/13 17:10
+ */
 @RestController
 @RequestMapping("userBase")
 @Api("用户基本信息")
